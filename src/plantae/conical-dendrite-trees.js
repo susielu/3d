1c0b1c7a83125691ffd160d41fb83760d87b8cae
James Womack <jwomack@netflix.com>
2016-04-29 16:27:33 -0400

    fix(git): Add canonical-dendrite

---
+++ src/plantae/conical-dendrite-trees.js
@@ -0,0 +1,50 @@
+export default [{
+    radius: 6,
+    sizeReduction: .85,
+    branchProbability: 0.12,
+    position: new THREE.Vector3(0, 0, 0),
+    rotation: new THREE.Vector3(0, 1, 0),
+    color: 'blue'
+}, {
+    radius: 3,
+    sizeReduction: .87,
+    branchProbability: 0.25,
+    position: new THREE.Vector3(10, 0, -10),
+    rotation: new THREE.Vector3(0,  1,  0),
+    color: 'blue'
+}, {
+    radius: 7,
+    sizeReduction: .82,
+    branchProbability: 0.22,
+    position: new THREE.Vector3(20, 0, 10),
+    rotation: new THREE.Vector3(0,  1,  0),
+    color: 'blue'
+},{
+    radius: 7,
+    sizeReduction: .82,
+    branchProbability: 0.22,
+    position: new THREE.Vector3(-20, 0, 5),
+    rotation: new THREE.Vector3(0,  1,  0),
+    color: 'blue'
+},{
+    radius: 7,
+    sizeReduction: .82,
+    branchProbability: 0.22,
+    position: new THREE.Vector3(-20, 0, -25),
+    rotation: new THREE.Vector3(0,  1,  0),
+    color: 'blue'
+},{
+    radius: 7,
+    sizeReduction: .92,
+    branchProbability: 0.18,
+    position: new THREE.Vector3(-15, 0, 15),
+    rotation: new THREE.Vector3(0,  1,  0),
+    color: 'pink'
+},{
+    radius: 10,
+    sizeReduction: .75,
+    branchProbability: 0.3,
+    position: new THREE.Vector3(-20, 0, -15),
+    rotation: new THREE.Vector3(0,  1,  0),
+    color: 'pink'
+}];
